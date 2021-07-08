8ec4e6ec688764bc4334f7e156cc5677c7844373
Justin Quaintance <justinquaintance@gmail.com>
2021-07-08 01:44:52 +0000

    getting deployment working

--- deploy.php
+++ deploy.php
@@ -1,93 +1,16 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
 
-// Forked from https://gist.github.com/1809044
-// Available from https://gist.github.com/nichtich/5290675#file-deploy-php
+// https://github.com/vicenteguerra/git-deploy
+define("TOKEN", "secret-token");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
+define("REMOTE_REPOSITORY", "git@github.com:slenderq/jquaintance.com.git"); // The SSH URL to your repository
+define("DIR", "/var/www/html/");                          // The path to your repostiroy; this must begin with a forward slash (/)
+define("BRANCH", "refs/heads/main");                                 // The branch route
+define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
+define("GIT", "/usr/bin/git");                                         // The path to the git executable
+define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
+define("BEFORE_PULL", "");                                             // A command to execute before pulling
+define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
 
-$TITLE   = 'Git Deployment Hamster';
-$VERSION = '0.11';
-
-echo <<<EOT
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>$TITLE</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
-  o-o    $TITLE
- /\\"/\   v$VERSION
-(`=*=') 
- ^---^`-.
-EOT;
-
-// Check whether client is allowed to trigger an update
-
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', '140.82.115.', // GitHub
-	'195.37.139.','193.174.' // VZG
-);
-$allowed = false;
-
-$headers = apache_request_headers();
-
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
-
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
-    exit;
-}
-
-flush();
-
-// Actually run the update
-
-$commands = array(
-	'echo $PWD',
-	'whoami',
-	'git pull',
-	'git status',
-	'git submodule sync',
-	'git submodule update',
-	'git submodule status',
-    'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
-);
-
-$output = "\n";
-
-$log = "####### ".date('Y-m-d H:i:s'). " #######\n";
-
-foreach($commands AS $command){
-    // Run it
-    $tmp = shell_exec("$command 2>&1");
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-
-    $log  .= "\$ $command\n".trim($tmp)."\n";
-}
-
-$log .= "\n";
-
-file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
-
-echo $output; 
-
-?>
-</pre>
-</body>
-</html>
+require_once("deployer.php");
