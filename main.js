0feebc872b6573ba6e542cbaa4ef284f2ee843fd
Justin Quaintance <justinquaintance@gmail.com>
2021-07-20 05:25:08 +0000

    mouse over events

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var main = new Vue({
-    el: '#main',
-    data: {
-        seen: true
-    }
-})
+
+function mouseOverMLDev() { console.log("hover!"); }
+function mouseOverProject() { }
+function mouseOverPhoto() { }
+function mouseOverLearn() { }
+function mouseLeave() { console.log("leave"); }
+
+
+function main_function() {
+    var main = new Vue({
+        el: '#main',
+        data: {},
+        methods: {
+            MouseLeave: mouseLeave,
+            MouseOverMLDev: mouseOverMLDev,
+            MouseOverProject: mouseOverProject,
+            MouseOverPhoto: mouseOverPhoto,
+            MouseOverLearn: mouseOverLearn,
+        }
+    })
+}
+
+main_function()
