bfcb3cd5603e797a60ea831b85c500a50093cce3
Justin Quaintance <justinquaintance@gmail.com>
2021-07-20 06:20:52 +0000

    trying to get hide working

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ function mouseOverMLDev() { }
 function mouseOverProject() { }
 
 function mouseOverPhoto() {
-    document.body.style.backgroundImage = "url('images/photos.jpg')"
+    document.body.style.visibility = "hidden";
+    document.getElementById("Photo").visibility = "visible";
+    document.body.style.backgroundImage = "url('images/photos.jpg')";
 }
 function mouseOverLearn() { }
 
 function mouseLeave() {
+    document.body.style.visibility = "visible"
     document.body.style.backgroundColor = "#000000";
     document.body.style.backgroundImage = "";
 }
