36d20d39417f19cd3ed75920b37727a5bf37d063
Justin Quaintance <justinquaintance@gmail.com>
2021-07-25 02:35:05 +0000

    getting the hover working !

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 
+function displayMouseOver(link_id, photo_src) {
+    document.body.style.visibility = "hidden";
+    document.getElementById(link_id).style.visibility = "visible";
+    document.body.style.backgroundImage = "url('" + photo_src + "')";
+}
+
+// test
 function mouseOverMLDev() { }
 
 function mouseOverProject() { }
 
 function mouseOverPhoto() {
-    // document.body.style.visibility = "hidden";
-    // document.getElementById("Photo").visibility = "visible";
-    document.body.style.backgroundImage = "url('images/photos.jpg')";
+    displayMouseOver("Photo", photos_img.src)
 }
 function mouseOverLearn() { }
 
 function mouseLeave() {
-    // document.body.style.visibility = "visible"
+    document.body.style.visibility = "visible"
     document.body.style.backgroundColor = "#000000";
     document.body.style.backgroundImage = "";
 }
@@ -32,4 +37,17 @@ function main_function() {
     })
 }
 
+function init() {
+    // get ready to display link....
+    document.body.style.backgroundImage = "";
+    photos_img = newImage('images/photos.jpg')
+}
+
+function newImage(src) {
+    img = new Image();
+    img.src = src;
+    return img;
+}
+
+init()
 main_function()
