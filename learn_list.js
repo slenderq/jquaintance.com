a4ccb1c73beff9268cdeec5ef68ca56922e6cd3a
Justin Quaintance <justinquaintance@gmail.com>
2021-09-19 19:19:15 +0000

    adding brea

--- learn_list.js
+++ learn_list.js
@@ -1,4 +1,6 @@
 
+// var fs = require('fs');
+
 function main() {
 
     list_id = "list"
@@ -13,14 +15,27 @@ function main() {
         }
     })
 
+    // https://stackoverflow.com/questions/31274329/get-list-of-filenames-in-folder-with-javascript
+    // var files = fs.readdirSync('/posts/')
+
+    // console.log(files)
+
     var title = "I did it"
     var date = "August 26th 2021"
-    append_to_list_container(list_id, title, date)
+    var link = "lol"
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
+    append_to_list_container(list_id, title, date, link)
 
 }
 
 
-function append_to_list_container(list_id, title, date) {
+function append_to_list_container(list_id, title, date, link) {
     // https://stackoverflow.com/questions/6840326/how-can-i-create-and-style-a-div-using-javascript
 
     var list_item_div = document.createElement("div");
@@ -37,6 +52,13 @@ function append_to_list_container(list_id, title, date) {
 
     title_div.appendChild(date_div);
     list_item_div.appendChild(title_div);
+
+    // adding and break
+    break_element = document.createElement("br");
+    list_item_div.appendChild(break_element);
+
     document.getElementById(list_id).appendChild(list_item_div);
+
+
 }
 main()
